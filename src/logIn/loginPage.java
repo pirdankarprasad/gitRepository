9b16631f66d1cef9aad80a56f52d8c17cba4c0ae
unknown <pirdankarprasad@gmail.com>
2024-01-21 11:46:59 +0530

    This is the first comment

---
+++ src/logIn/loginPage.java
@@ -0,0 +1,11 @@
+package logIn;
+
+public class loginPage {
+	public void logintoApplication() {
+		System.out.println("Login to the applicatiom");
+	}
+	
+	public void logout() {
+		System.out.println("Logout from application");
+	}
+}
